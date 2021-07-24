62d7fb2562b860de4ce12fd856fb1d60d6cdc902
mayoalexnder <admin@freelabel.net>
2021-07-24 11:11:31 -0500

    created search query + filtering functionality

--- src/views/Contacts/ListAll.vue
+++ src/views/Contacts/ListAll.vue
@@ -1,8 +1,19 @@
 <template lang="html">
-  <div class="">
+  <div class="container">
     <h4>this is the list all</h4>
     <div class="all-contacts">
-      allContacts {{ allContacts }}
+      <div class="controls row">
+        <div class="col-md-9">
+          <b-form-input v-model="searchQuery" placeholder="Search anything.."></b-form-input>
+        </div>
+        <div class="col-md-3">
+          <b-button block class="w-100" @click="search()">
+            Search
+          </b-button>
+        </div>
+      </div>
+
+      <b-table striped hover :items="filteredContacts"></b-table>
     </div>
   </div>
 </template>
@@ -12,7 +23,8 @@ import contacts from "@/data/contacts.json"
 export default {
   data () {
     return {
-      allContacts: []
+      allContacts: [],
+      searchQuery: ''
     }
   },
   mounted () {
@@ -20,6 +32,46 @@ export default {
     console.log({
       contacts
     })
+  },
+  computed: {
+    filteredContacts () {
+      let filtered = this.allContacts
+      const query = this.searchQuery.trim().toLowerCase()
+      if (query) {
+        filtered = this.allContacts.filter((item) => {
+          // need to pull all data before removing
+          console.log({
+            item
+          })
+          let res = false
+          if (item.name.toLowerCase().indexOf(query) > -1) {
+            res = true
+          }
+          if (item.email
+            && item.email.toLowerCase().indexOf(query) > -1) {
+            res = true
+          }
+          if (item.city
+            && item.city.toLowerCase().indexOf(query) > -1) {
+            res = true
+          }
+          if (item.state
+            && item.state.toLowerCase().indexOf(query) > -1) {
+            res = true
+          }
+          if (item.postal_code
+            && item.postal_code.toString().toLowerCase().indexOf(query) > -1) {
+            res = true
+          }
+          if (item.keywords
+            && item.keywords.toLowerCase().indexOf(query) > -1) {
+            res = true
+          }
+          return res
+        });
+      }
+      return filtered
+    },
   }
 }
 </script>
